85d2389fe2fad5a63183b0de8678ec50839c4d8e
Yevheniia Nezhalska <Yevheniia.Nezhalska@teaminternational.com>
2022-01-23 15:07:24 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin({}),
+            new TerserPlugin()
+        ]
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 4200
+    },
+    plugins: [
+        new HTMLPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    }
+}
